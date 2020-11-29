33e1724a154e3ce35e1d13b60d8a77d658a31f81
master8 <master8.michael@gmail.com>
2020-11-29 23:55:52 +0300

    remove unused styles

--- app/src/main/java/com/master8/kino/ui/MainActivity.kt
+++ app/src/main/java/com/master8/kino/ui/MainActivity.kt
@@ -3,12 +3,8 @@ package com.master8.kino.ui
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
 import androidx.compose.ui.platform.setContent
-import androidx.ui.tooling.preview.Preview
 import com.master8.kino.data.source.tinkoff.createInvestApiService
 import com.master8.kino.domain.entity.Instrument
 import com.master8.kino.domain.entity.PortfolioPosition
@@ -49,29 +45,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            キノTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Text("Hello world!")
         }
 
         positions.forEach {
             Log.e("mv8", "result $it ${it.expectedYield} ${it.expectedYieldInPercent}")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    キノTheme {
-        Greeting("Android")
     }
 }
